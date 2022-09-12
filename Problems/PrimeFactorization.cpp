a677c313ffdbd80d50792463c56ddb367728eceb
Dinesh2010-coder <dinesh0206.be20@chitkara.edu.in>
2022-09-12 13:44:45 +0530

    Prime updated

--- Problems/PrimeFactorization.cpp
+++ Problems/PrimeFactorization.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include<math.h>
 
 using namespace std;
-
+/*
 void primeFactors(int n){
     int i=2;
     while(n>1){
@@ -14,11 +15,35 @@ void primeFactors(int n){
         }
     }
 }
+*/
+
+void primeFactors(int n){
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n%i==0)
+        {
+        int count = 0;
+            while (n%i==0)
+            {
+                count++;
+                n = n/i;
+            }
+            cout<<i<<"^" <<count<<endl;
+            
+        }
+        
+    }
+    if(n>1){
+        cout<<n<<"^"<<1<<endl;
+    }
+    
+}
 
 int main()
 {
     int n;
     cin>>n;
     primeFactors(n);
+    
     return 0;
 }
