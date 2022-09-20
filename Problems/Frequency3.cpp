9175a9a575006f9794a56677b69b63731f26c9ca
Dinesh2010-coder <dinesh0206.be20@chitkara.edu.in>
2022-09-20 23:16:51 +0530

    Frequency3 updated

--- Problems/Frequency3.cpp
+++ Problems/Frequency3.cpp
@@ -4,28 +4,35 @@
 using namespace std; 
 
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int t;
+    cin>>t;
+    while (t--)
     {
-        cin>>arr[i];
-    }
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>arr[i];
+        }
 
     
-    unordered_map<int,int> visited;
+        unordered_map<int,int> visited;
 
-    for(int i=0;i<n;i++){
-        visited[arr[i]]++;
-    }
-
-    for(auto i:visited){
-        if (i.second != 3)
-        {
-            cout<<i.first<<endl;
+        for(int i=0;i<n;i++){
+            visited[arr[i]]++;
         }
+
+        for(auto i:visited){
+            if (i.second != 3)
+            {
+                cout<<i.first<<endl;
+            }
         
+        }
     }
+    
+    
     
     return 0;
 }
